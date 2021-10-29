ee7bec91a8c48e720f175dbc2d3334af16597e5e
RalGoX1 <293346@viauc.dk>
2021-10-29 11:44:50 +0200

    Updated Controller /users/{username}

--- src/main/java/com/example/Food4U/Controller/UserController.java
+++ src/main/java/com/example/Food4U/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.Food4U.Controller;
 import com.example.Food4U.Model.User;
 import com.example.Food4U.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +23,12 @@ public class UserController
   @PostMapping("/users")
   public User createUser(@RequestBody User user) {
     return userRepository.save(user);
+  }
+
+  @GetMapping("/users/{username}")
+  public ResponseEntity<User> getEmployeeById(@PathVariable(value = "username") String userUsername)
+      throws Exception {
+      User employee = userRepository.findById(userUsername).orElseThrow(() -> new Exception("User not found"));
+    return ResponseEntity.ok().body(employee);
   }
 }
